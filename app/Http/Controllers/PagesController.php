5d467ca1437f86f1ed01b2a4b96df660ea324267
lanabandr <lanabandaryl@gmail.com>
2016-11-03 18:09:42 +0800

    Redirect to Dashboard after Login

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -19,7 +19,7 @@ class PagesController extends Controller
 	
 	public function login()
 	   {
-	   	return view('login');
+	   	return view('pages.dashboard');
 	   }
 
    	public function dashboard()
