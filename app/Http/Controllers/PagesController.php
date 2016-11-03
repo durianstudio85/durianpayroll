8e2ab0fdde478ef69e13ad8fed33e8af19da27b2
lanabandr <lanabandaryl@gmail.com>
2016-11-03 12:40:23 +0800

    Changed Login Page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace App\Http\Controllers\Auth;
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
@@ -11,6 +11,12 @@ use DB;
 
 class PagesController extends Controller
 {
+
+	public function __construct()
+	    {
+	     $this->middleware('auth');
+	    }
+
 	public function login()
 	   {
 	   	return view('auth.login');
