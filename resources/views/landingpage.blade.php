0a8315cc6876de9149d79c5d2df7ac7b1ef5b13f
Eduardo Barrete <eduardo@alsocreative.com>
2016-11-23 17:38:10 +0800

    Updates
    Updates on landing page

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -6,12 +6,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/helper.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/pe-icon-7-stroke.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.css') }}">
-<script src="{{ asset('js/Chart.min.js') }}"></script>
+<script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script><!-- Tether for Bootstrap --> 
+
 
 
 <body bgcolor="#f1f3f6" style="overflow-x: hidden;">
@@ -54,16 +55,16 @@
 			<div class="landing-bt">
 				<center>
 					<a href="{{ url('/login') }}">
-					<div style="width: 430px; height: 101px; background-color: #1fb4ae;">
-		
-						<div class="landing-bt-inner-1">
-							<p class="landing-bt-text1">GET STARTED NOW</p>
-							<p class="landing-bt-text2">NO COMMITMENT CREDIT CARD</p>
+						<div style="width: 430px; height: 101px; background-color: #1fb4ae;">
 
-							<i class="fa fa-chevron-circle-right" aria-hidden="true"></i>
-						</div>
+							<div class="landing-bt-inner-1">
+								<p class="landing-bt-text1">GET STARTED NOW</p>
+								<p class="landing-bt-text2">NO COMMITMENT CREDIT CARD</p>
+
+								<i class="fa fa-chevron-circle-right" aria-hidden="true"></i>
+							</div>
 							
-					</div>
+						</div>
 					</a>
 				</center>
 			</div>
@@ -89,67 +90,159 @@
 
 	</section>
 
-	<!-- <section class="secondsection">
+	<section class="secondsection">
 
+		<h1>Friendly, easy to use navigation for small businesses</h1>
+		
+		<center>
+			<div class="header-line">
+				
+				<img src="{{ asset('images/Rectangle.png') }}">
 
+				<div class="container">
 
+					<div class="row col-div-3">
+						<div class="col-sm-6 col-md-4">
+							<div class="section1divs">
+								<div class="padleftright">
+									<img src="{{ asset('images/section1-1.png') }}">
 
+									<h4>Quickly setup your company and manage its data online</h4>
+									<p>Salarium is easy to setup for small and medium businesses with little or no HR<br> support.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-4">
+							<div class="section1divs">
+								<div class="padleftright">
+									<img src="{{ asset('images/section1-2.png') }}">
 
+									<h4>Enrolling new employees takes seconds</h4>
+									<p>Each employee has a 201 file that allows for easy data maintenance and access<br> anywhere in the world.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-4">
+							<div class="section1divs">
+								<div class="padleftright">
+									<img src="{{ asset('images/section1-3.png') }}">
+
+									<h4>Pay your employees in five seconds or less</h4>
+									<p>The beauty of having an automated payroll system is that processing payroll is<br> just a few clicks away.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+		</center>
 	</section>
 
 	<section class="thirdsection">
 
+		<div class="container">
 
+			<div class="col-md-6">
 
+				<div id="carousel-example-generic" class="carousel slide margtop100" data-ride="carousel" >
+					<!-- Indicators -->
+					<ol class="carousel-indicators margtop100">
+						<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+					</ol>
+
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner" role="listbox">
+						<div class="item active">
+							<img src="{{ asset('images/PC-Slide.png') }}" alt="...">
+							
+						</div>
+						<div class="item">
+							<img src="{{ asset('images/PC-Slide.png') }}" alt="...">
+							
+						</div>
+						<div class="item">
+							<img src="{{ asset('images/PC-Slide.png') }}" alt="...">
+							
+						</div>
+					</div>
+
+					<!-- Controls -->
+					<a style="display: none;" class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a style="display: none;" class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+
+			</div>
+
+			<div class="col-md-6">
+
+				<div class="margtop100">
+					
+					
+
+				</div>
+				
+			</div>
+
+		</div>
 
 	</section>
 
-	<section class="fourthsection">
+	<!-- <section class="fourthsection">
+		
+
+
+
+</section> -->
+
+	<!-- <section class="fifthsection">
+		
+
+
+
+</section> -->
+
+	<!-- <section class="sixthsection">
+		
+
+
+
+</section> -->
+
+	<!-- <section class="seventhsection">
+		
+
+
+
+</section> -->
+
+	<!-- <section class="eightsection">
+		
+
+
+
+</section> -->
+
+	<!-- <section class="ninthsection">
 		
 
 
 
 	</section>
-
-	<section class="fifthsection">
-		
+-->
 
 
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
-	</section>
-
-	<section class="sixthsection">
-		
-
-
-
-	</section>
-
-	<section class="seventhsection">
-		
-
-
-
-	</section>
-
-	<section class="eightsection">
-		
-
-
-
-	</section>
-
-	<section class="ninthsection">
-		
-
-
-
-	</section> -->
-
-
-
-	<script src="{{ asset('js/jquery-1.9.1.min.js') }}"></script>
-	<script src="{{ asset('js/bootstrap.js') }}"></script>
-	<script src="{{ asset('js/npm.js') }}"></script>
 </body>
 </html>
