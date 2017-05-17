c91b85c248edae98ac4021d61409480e94d966aa
jbumbay159 <jaybeeumbay159@outlook.com>
2017-05-17 10:38:47 +0800

    Favicon
    Update Favicon

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -4,6 +4,7 @@
 	<title>Durian Payroll</title>
 </head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="{{{ asset('images/favicon.png') }}}">
 
 <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.css') }}">
