8351a00c6d2bf73664e366d753bc7af0a77fcfc1
jbumbay159 <jaybeeumbay159@outlook.com>
2017-05-27 16:13:25 +0800

    Update the button
    update button

--- resources/views/header_footer.blade.php
+++ resources/views/header_footer.blade.php
@@ -173,14 +173,15 @@
                         
                             <div class="form-group">
                                 <div class="col-sm-2">
+                                    {!! Form::text('pay_cycle', null,['class'=>'form-control', 'placeholder'=>'Select Pay Cycle']) !!}
+                                </div>
+                                <div class="col-sm-2">
                                     {!! Form::date('date_start', null,['class'=>'form-control', 'placeholder'=>'Date Start']) !!}
                                 </div>
                                 <div class="col-sm-2">
                                     {!! Form::date('date_end', null,['class'=>'form-control', 'placeholder'=>'Date Start']) !!}
                                 </div>
-                                <div class="col-sm-2">
-                                    {!! Form::text('pay_cycle', null,['class'=>'form-control', 'placeholder'=>'Select Pay Cycle']) !!}
-                                </div>
+                                
                             </div>
                             <div class="table-responsive">
                             <table width="100%" class="table table-striped table-hover">
